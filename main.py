0f8f1d37eb71eb835210ca44c1f1326e2ea8b0bd
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2020-12-30 11:54:17 -0300

    BG and Floor loaded + floor moving

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame, sys
 
+def draw_floor():
+    global floor_x_pos
+    screen.blit(floor_sf, (floor_x_pos,450))
+    screen.blit(floor_sf, (floor_x_pos+SCREEN_SIZE[0],450))
+    floor_x_pos -= 1
+    if floor_x_pos <= -SCREEN_SIZE[0]:
+        floor_x_pos = 0
+
 pygame.init()
 SCREEN_SIZE = (288,512)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
+
+# Variables
+floor_x_pos = 0
+
+background_sf = pygame.image.load("sprites/background-day.png").convert()
+floor_sf = pygame.image.load("sprites/base.png").convert()
 
 while True:
     for event in pygame.event.get():
@@ -11,5 +25,8 @@ while True:
             pygame.quit()
             sys.exit()
     
+    screen.blit(background_sf, (0,0))
+    draw_floor()
+
     pygame.display.update()
     clock.tick(120)
